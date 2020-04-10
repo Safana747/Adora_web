7b6941c93a2b56bc2b9dd074932774aec1d769a5
sabique <sabiquehiltop@gmail.com>
2020-04-10 11:02:38 +0530

    style changes

--- resources/views/site/our_brands.blade.php
+++ resources/views/site/our_brands.blade.php
@@ -22,7 +22,7 @@
                     @foreach($brands as $item)
                      <div class="col-lg-6 brand_s">
                     <div class="media">
-                        <img src="{{url('assets/images/brands/'.$item->image)}}" class="mr-3 " alt="" title="">
+                        <img src="{{url('assets/images/brands/'.$item->image)}}" class="mr-3 img-object-contain" alt="" title="">
                         <div class="media-body">
                             <p>
                                 {{$item->description}}
